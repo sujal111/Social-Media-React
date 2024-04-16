9f9323998a0fd22601d02b996116e639fa53a501
sujalmaiti <sujalmaiti123456@gmail.com>
2024-04-16 23:47:23 +0530

    social media app initiated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Button from '@mui/material/Button';
+import Navbar from './Navbar'
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+  <Button variant="contained">Hello world</Button>;
+  
+    </>
+  )
+}
+
+export default App
